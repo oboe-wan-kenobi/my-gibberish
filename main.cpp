651a187b3421e73d874f402f0a9ce01a59a21209
Josiah Whitlock <1964969796@hcs-students.net>
2020-10-13 22:41:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <locale>
+#include <string>
+using namespace std;
+
+int main() {
+  
+	string a; 
+	// string is a data type. So, each variable has a data type. int (number without a decimal), bool (true or false), string (text in double quotes), float (number with decimal, always followed by an f, so like float i = 0.9f), double (float but two times bigger) byte (small number), short (bigger than byte, smaller than everything else), long (bigger than int)
+
+	cout << "DO YOU WANT TO PLAY A GAME?" << endl;
+	//output, whatever is in between the <<s will be outputted to the console
+
+	cin >> a;
+	//input, whatever is typed into the console before the enter key is next pressed will be stored in the variable specified after the >>
+
+	string listOfGames[6] = { "CHESS", "CHECKERS", "BACKGAMMON", "POKER", "THEATERWIDE BIOTOXIC AND CHEMICAL WARFARE", "GLOBAL THERMONUCLEAR WARFARE" };
+
+	//mwahahaha i added back the space because its easier to read
+	if  (a == "yes") {
+	 
+		for (int game = 0; game < 6; game++) {
+
+			//variable++ is the same as variable = variable + 1;
+			//or variable += 1;
+
+			cout << listOfGames[game] << "\n";
+
+		}
+
+	} else if (a == "YES") {
+
+  	for (int game = 0; game < 6; game++) {
+
+			cout << listOfGames[game] << "\n";
+ 
+		}
+
+	} if (a == "Yes") {
+
+ 		for (int game = 0; game < 6; game++) {
+
+   	 cout << listOfGames[game] << "\n";
+
+		}
+
+	} if (a == "no") {
+ 
+ 		cout << "Go die in a hole. \n\nDo something much less interesting than play my fun games";
+
+	} if (a == "NO") {
+ 
+	 	cout << "Go die in a hole. \n\nDo something much less interesting than play my fun games";
+ 
+	} if (a == "nO") {
+ 
+ 		cout << "Go die in a hole. \n\nDo something much less interesting than play my fun games";
+ 
+	} if (a == "No") {
+ 
+ 		cout << "Go die in a hole. \n\nDo something much less interesting than play my fun games";
+ 
+	}
+
+  return(0);
+
+}
